52200cd44fc959aecf657603c3bd07a2c5d9aa62
Nick Merrill <nicholas.e.merrill@gmail.com>
2013-04-29 15:51:45 -0400

    More work on loading stocks.

--- MAIN.R
+++ MAIN.R
@@ -1,18 +1,36 @@
 stocks = c("AAPL", "CAT", "SUN")
 
-load.stock.data.frame = function() {
-  stock.data = c()
+# for testing only
+getSymbols(stocks)
+
+date.from = "2010-01-01"
+date.to = "2013-01-01"
+
+basis = getSymbols("AAPL", auto.assign=F, from=date.from, to=date.to)
+dates = index(basis)
+
+load.stock.data = function() {
   num.stocks = length(stocks)
+  num.days = length(dates)
+  stock.data = data.frame()
+  # create matrix that allows for num.days days of trading data
+  stock.data = matrix(nrow=num.days, ncol=num.stocks, dimnames=list(1:num.days, stocks))
+  stock.data
   for (i in 1:num.stocks) {
-    stock.data = c(stock.data, getSymbols(stocks[i], auto.assign=F))
+    # load the adjusted price of each stock into the matrix
+    adj = Ad(getSymbols(stocks[i], auto.assign=F, from=date.from, to=date.to))
+    #### WATCH OUT! DATES DON'T MATCH ALWAYS. LOOK AT (SUN) ###
+    print(length(adj))
+    for (j in 1:num.days) {
+      stock.data[j,i] = adj[j]
+    }
   }
-  stock.data.frame = data.frame(stock.data[,1])
-  print(stock.data.frame)
-  return(stock.data.frame)
+  print(stock.data)
+  return(stock.data)
 }
 
 # load stock data into environment
-stock.data.frame = get.stock.data.frame()
+stock.data = get.stock.data()
 
 stop("Done")
 
